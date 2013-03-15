3bb5c64fd479eff25c3449e9fa6faf615bde44b0
Vladimir <solvik74@mail.ru>
2013-03-15 22:43:19 +0600

    Revert "LastModified(15.03.2013)"
    This reverts commit cf6b068c03f5e91a225c6efefd0fc8c94eb19832.

--- index.php
+++ index.php
@@ -9,32 +9,18 @@
 <meta name="" content="">
 
 <link rel="stylesheet" type="text/css" href="css/big.css" />
-<script src="js/jquery-1.4.2.js"></script>
-<script src="js/jquery.hotkeys.js"></script>
 <script src="js/big.js"></script>
-
 </head>
 <body onload="fillTextarea();">
-	<table cellspacing="10">
+	<table>
 		<tr>
-			<td width="600">
-				Имя страницы (например cat.php): <input type="text" name="page_name" id="page_name" value="newpage.php" />
-				<input type="submit" value="R" onClick="fillTextarea();" />
-				<br />Title: <input type="text" name="page_title" id="page_title" value="newpage" /><br /><br />
-				Код:<br /><br />
-				<textarea id="page_code" name="page_code" class="big-textarea" onkeydown="insertTab(event, this);"></textarea>
+			<td width="1000">
+				Имя страницы (например cat.php): <input type="text" name="page_name" id="page_name" /><br />Title: <input type="text" name="page_title" id="page_title" /><br />
+				Код:<br />
+				<textarea id="page_code" name="page_code" class="big-textarea"></textarea>
 				<a href="javascript:void(0);" onclick="createNewPage();">Сохранить</a>
-				<a href="javascript:void(0);" onclick="fillTextarea();">Обновить</a>
+				<a href="javascript:void(0);" onclick="fillTextarea();">Обновьить</a>
 				<a href="javascript:void(0);" onclick="insertHtmlAtCursor();">Вставить код</a>
-				<div id="info">Документ успешно сохранен.</div>
-			</td>
-			<td>
-				<div id="hide-code-frame">
-					Просмотр кода онлайн:<br />
-					<p id="red-info"></p>
-					<iframe src="index.php" name="save-file" id="save-file" >					
-					</iframe>
-				</div>
 			</td>
 		</tr>
 	</table>
--- js/big.js
+++ js/big.js
@@ -1,5 +1,3 @@
-var last_code = '';
-var last_cursor_pos = 0;
 var page_title = '';
 
 
@@ -46,207 +44,30 @@ function createNewPage()
 function fillTextarea()
 {
 	var page_title = "<title>" + document.getElementById('page_title').value + "</title>\n";
-	var page_name = document.getElementById('page_name').value
 	
-	if (page_name == '')
-	{
-		
-		var body_code = 
-		'<table>\n'+
-		'  <tr>\n'+
-		'     <td>\n'+
-		'        \n'+
-		'     </td>\n'+
-		'     <td>\n'+
-		'        \n'+
-		'     </td>\n'+
-		'  </tr>\n'+	
-		'</table>\n';
-		
-		var page_code = document.getElementById('page_code');
-		page_code.value =
-		'<?php include("../parts/config/connect.php"); ?>\n\n'+ 
-		'<!doctype html>\n'+
-		'<html>\n\n'+
-		'<head>\n'+
-		page_title+
-		'</head>\n\n\n'+
-		'<body>\n\n'+
-		body_code+
-		'\n'+
-		'</body>\n\n'+
-		'</html>\n';
-	}
-	else
-	{
-		var page_code = document.getElementById('page_code');
-		page_code.value = getFile(page_name);
-	}
+	var body_code = 
+	'<table>\n'+
+	'  <tr>\n'+
+	'     <td>\n'+
+	'         \n'+
+	'     </td>\n'+
+	'     <td>\n'+
+	'         \n'+
+	'     </td>\n'+
+	'  </tr>\n'+	
+	'</table>\n';
 	
-	last_code = page_code.value;
-}
-
-function getFile(name)
-{
-	var req = getXmlHttp(); 
-	var page_name = document.getElementById('page_name').value;
-	var page_code = document.getElementById('page_code').value;
-	var req_text = '';
-	
-	req.onreadystatechange = function() {  
-        // onreadystatechange активируетс€ при получении ответа сервера
-		if (req.readyState == 4) { 
-            // если запрос закончил выполн€тьс€
-			if(req.status == 200) { 
-				req_text = req.responseText;
-			}
-		}
-	}
-	req.open('GET', 'pages/'+name, false);  
-	req.send(null);
-	
-	return req_text;
-}
-
-function insertTab(evt, obj) 
-{	
-    evt = evt || window.event;
-    var keyCode = evt.keyCode || evt.which || 0;
-    
-    if(keyCode == 9)
-    {
-        if(document.selection)
-        {															
-            document.selection.createRange().duplicate().text = "\t";
-        }
-        else if(obj.setSelectionRange)
-        {				
-            var strFirst = obj.value.substr(0, obj.selectionStart);
-            var strLast  = obj.value.substr(obj.selectionEnd, obj.value.length);
-
-            obj.value = strFirst + "\t" + strLast;
-       
-            var cursor = strFirst.length + "\t".length;
-
-            obj.selectionStart = obj.selectionEnd = cursor;
-        }
-        
-        if(evt.preventDefault && evt.stopPropagation)
-        {
-            evt.preventDefault();
-            evt.stopPropagation();
-        }
-        else {
-            evt.returnValue = false;
-            evt.cancelBubble = true;
-        }
-        
-        return false;
-    }
-	
-	if ((!evt.ctrlKey) && (!evt.altKey))
-	document.getElementById('red-info').innerHTML = '<font color="red">— момента последней загрузки код был изменен. <br />Ќажмите <b>Ctrl+S</b> дл€ просмотра текущего состо€ни€...</font>';	
-}
-
-function replaceOrInsertHTML(obj,evt,textHTML,cursorPos)
-{
-	last_code = document.getElementById('page_code').value;
-	
-		evt.preventDefault();
-        if(document.selection)
-        {															
-            document.selection.createRange().duplicate().text = textHTML;
-        }
-        else if(obj.setSelectionRange)
-        {				
-            var strFirst = obj.value.substr(0, obj.selectionStart);
-            var strLast  = obj.value.substr(obj.selectionEnd, obj.value.length);
-
-            obj.value = strFirst + textHTML + strLast;
-       
-            var cursor = strFirst.length + cursorPos;
-
-            obj.selectionStart = obj.selectionEnd = cursor;
-        }
-        
-        if(evt.preventDefault && evt.stopPropagation)
-        {
-            evt.preventDefault();
-            evt.stopPropagation();
-        }
-        else {
-            evt.returnValue = false;
-            evt.cancelBubble = true;
-        }
-	return false;
-}
-
-function backtoLastCode()
-{
 	var page_code = document.getElementById('page_code');
-		page_code.value = last_code;	
-	page_code.selectionStart = page_code.selectionEnd = last_cursor_pos;
+	page_code.value =
+	'<?php include("../parts/config/connect.php"); ?>\n\n'+ 
+	'<!doctype html>\n'+
+	'<html>\n\n'+
+	'<head>\n'+
+	page_title+
+	'</head>\n\n\n'+
+	'<body>\n\n'+
+	body_code+
+	'\n'+
+	'</body>\n\n'+
+	'</html>\n';
 }
-
-$(document).ready(function()
-{
-	$('#page_code').bind('keydown', 'alt+d', function(e){
-		replaceOrInsertHTML(this,e,'<div id="" name=""></div>',9);
-	});
-	
-	$('#page_code').bind('keydown', 'alt+p', function(e){
-		replaceOrInsertHTML(this,e,'<?php  ?>',6);
-	});	
-	
-	$('#page_code').bind('keydown', 'alt+e', function(e){
-		replaceOrInsertHTML(this,e,'<?php echo ""; ?>',12);
-	});		
-	
-	$('#page_code').bind('keydown', 'alt+i', function(e){
-		replaceOrInsertHTML(this,e,'<?php include(""); ?>',15);
-	});
-	
-	$('#page_code').bind('keydown', 'alt+s', function(e){
-		replaceOrInsertHTML(this,e,'<link rel="stylesheet" href="" type="text/css" />',29);
-	});
-	
-	$('#page_code').bind('keydown', 'ctrl+z', function(e){
-		backtoLastCode();
-	});
-	
-	$('#page_code').bind('keydown', 'ctrl+s', function(e){
-		e.preventDefault();
-		
-		
-		
-		var page_name = $('#page_name').val();
-		var page_code = $('#page_code').val();
-		
-		for (var i=1; i<page_code.length-1; i++)
-		{
-			page_code = page_code.replace('\n','[nl]');
-			page_code = page_code.replace('\t','[tab]');
-		}
-		
-		$.ajax({
-			url: 'parts/admin/create-page.php?page_name='+page_name+'&page_code='+page_code
-		});
-		
-		$('#red-info').html('');
-		
-		setTimeout(function(){
-			$('#save-file').attr('src','pages/'+page_name);
-			$('#hide-code-frame').fadeIn("fast");
-		},100);
-		
-		$('#info').fadeIn("fast");
-		setTimeout(function(){ $('#info').fadeOut("fast"); }, 2000);
-		
-	});
-	
-	$('#info').click(function(){
-		
-	});
-	
-	
-});
